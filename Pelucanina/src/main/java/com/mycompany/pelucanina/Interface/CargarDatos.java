a22866fd005136d8fc92761bbf6dfaf12fa934c1
miqueas04 <80845205+miqueas04@users.noreply.github.com>
2022-10-25 17:02:47 -0300

    funcionalidadbtn
    funcionalidad al boton limpiar

--- Pelucanina/src/main/java/com/mycompany/pelucanina/Interface/CargarDatos.java
+++ Pelucanina/src/main/java/com/mycompany/pelucanina/Interface/CargarDatos.java
@@ -284,7 +284,16 @@ public class CargarDatos extends javax.swing.JFrame {
     }//GEN-LAST:event_btnGuardarActionPerformed
 
     private void btnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarActionPerformed
-        // TODO add your handling code here:
+       
+        txtNombre.setText("");
+        txtColor.setText("");
+        txtRaza.setText("");
+        txtCelDuenio.setText("");
+        txtNombreDuenio.setText("");
+        txtObservaciones.setText("");
+        cmbAlergico.setSelectedIndex(0);
+        cmbAtencionEspecial.setSelectedIndex(0);
+        
     }//GEN-LAST:event_btnLimpiarActionPerformed
 
     
